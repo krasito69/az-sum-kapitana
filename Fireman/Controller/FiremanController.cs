b6bc9c340ec8ba7dadaf3620c087a81e1fed3b74
krasito69 <85482895+krasito69@users.noreply.github.com>
2021-06-07 11:21:16 +0300

    Add files via upload

---
+++ Fireman/Controller/FiremanController.cs
@@ -0,0 +1,39 @@
+﻿using Fireman.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fireman.Controller
+{
+    public class FiremanController
+    {
+        public List<Fireman.Model.Fireman> GetFireman()
+        {
+            using (FiremenDBEntities f = new FiremenDBEntities())
+            {
+                return f.Firemen.ToList();
+            }
+        }
+
+        public void InsertFireman(Fireman.Model.Fireman fireman)
+        {
+            using (FiremenDBEntities f = new FiremenDBEntities())
+            {
+                fireman.Id = f.Firemen.Count() + 1;
+                f.Firemen.Add(fireman);
+                f.SaveChanges();
+            }
+        }
+
+        public List<Fireman.Model.Fireman> OrderByIncident()
+        {
+            using (FiremenDBEntities fm = new FiremenDBEntities())
+            {
+                return fm.Firemen.OrderByDescending(f => f.IncidentsPartiicipated).ToList();
+            }
+        }
+
+    }
+}
